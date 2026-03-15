6c1018fdb0834b7dba2f2f5565421a99a10cd9ef
Mirr218 <st.pt01@mail.ru>
2026-03-15 21:48:20 +0600

    correct test

--- stack_test.c
+++ stack_test.c
@@ -1,28 +1,53 @@
 #include "stack.h"
-#include <stdio.h>
 
 int main() {
   Stack s;
   init_stack(&s);
 
-  if (is_empty(&s)) {
-    printf("Stack is empty: OK\n");
+  if (!is_empty(&s)) {
+    return 1;
   }
 
-  push(&s, 10);
-  push(&s, 20);
+  if (!push(&s, 10)) {
+    return 1;
+  }
+  if (!push(&s, 20)) {
+    return 1;
+  }
+
+  if (is_empty(&s)) {
+    return 1;
+  }
 
   int val;
-  if (pop(&s, &val) && val == 20) {
-    printf("Pop 20: OK\n");
-  }
-  if (pop(&s, &val) && val == 10) {
-    printf("Pop 10: OK\n");
-  }
-  if (is_empty(&s)) {
-    printf("Stack is empty again: OK\n");
+
+  if (!pop(&s, &val) || val != 20) {
+    return 1;
   }
 
-  printf("All tests passed!\n");
+  if (!pop(&s, &val) || val != 10) {
+    return 1;
+  }
+
+  if (!is_empty(&s)) {
+    return 1;
+  }
+
+  init_stack(&s);
+  int i;
+  for (i = 0; i < MAX_SIZE; ++i) {
+    if (!push(&s, i)) {
+      return 1;
+    }
+  }
+  if (push(&s, 123)) {
+    return 1;
+  }
+
+  init_stack(&s);
+  if (pop(&s, &val)) {
+    return 1;
+  }
+
   return 0;
 }
