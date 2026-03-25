bbd10f10332313483bb9c3fc82cc86ed1b74100e
aazhdanova55 <aazhdanova55@mail.ru>
2026-03-25 19:37:30 +0600

    Автоматическая проверка

--- list_test.c
+++ list_test.c
@@ -3,34 +3,43 @@ int main() {
   struct Node *start = NULL;
   int k, x, n;
 
-  printf("1 - Добавление в начало 3,2,1\n");
+  // 1 - Добавление в начало 3,2,1;
   start = f1(start, 3);
   start = f1(start, 2);
   start = f1(start, 1);
-  f7(start);
+  if (f5(start, 0) != 1 || f5(start, 1) != 2 || f5(start, 2) != 3) {
+    return 1;
+  }
 
-  printf("2 - Добавление в конец 5,6\n");
+  // 2 - Добавление в конец 5,6
   start = f2(start, 5);
   start = f2(start, 6);
-  f7(start);
+  if (f5(start, 3) != 5 || f5(start, 4) != 6) {
+    return 1;
+  }
 
-  printf("3 - Добавление 4 по номеру 3\n");
+  // 3 - Добавление 4 по номеру 3
   start = f3(start, 4, 3);
-  f7(start);
+  if (f5(start, 3) != 4) {
+    return 1;
+  }
 
-  printf("4 - Удаление по  номеру 5\n");
-  start = f4(start, 5);
-  f7(start);
+  // 4 - Удаление по  номеру 0
+  start = f4(start, 0);
+  if (f5(start, 0) != 2) {
+    return 1;
+  }
 
-  printf("5 - Найти по  номеру 0\n");
-  x = f5(start, 0);
-  printf("Элемент на позиции 0: %d\n", x);
+  // 5 - Найти по  номеру 1
+  x = f5(start, 1);
+  if (x != 3) {
+    return 1;
+  }
 
   n = f6(start);
-  printf("Количество элементов: %d\n", n);
-
-  printf("7 - Список\n");
-  f7(start);
+  if (n != 5) {
+    return 1;
+  }
 
   return 0;
 }
